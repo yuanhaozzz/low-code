6cf41f15a6a51c77b7494baa36fa7e3be5212eb2
yuanhao <yuanhao_zzz@163.com>
2022-11-22 20:10:04 +0800

    deploy

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const fs = require("fs");
+
+app.use(express.static(path.resolve(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  const html = fs.readFileSync(
+    path.resolve(__dirname, "dist/my-home/index.html"),
+    "utf-8"
+  );
+  res.send(html);
+});
+
+app.listen(3005, () => {
+  console.log("start 3005");
+});
